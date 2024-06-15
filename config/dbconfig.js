213b8a5957d9dd740cd421ae7ef2a7313ef5ef0c
Bitthal verma <87093213+bcoder4702@users.noreply.github.com>
2024-06-15 15:38:22 +0530

    setting up dbconfig

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: process.env.DB_HOST || 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'bitespeedproject',
+    USER: process.env.DB_USERNAME || 'root',
+    PASSWORD: process.env.DB_PASSWORD || '',
+    DB: process.env.DB_DBNAME || 'bitespeedproject',
     dialect: 'mysql',
 
     pool: {
